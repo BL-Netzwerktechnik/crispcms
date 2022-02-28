ebb311bbb5e17f103c99b55962cd8fc0df265d2a
Justin Back <j.back@jrbit.de>
2022-02-28 19:13:09 +0100

    Add HookFile.php
    Signed-off-by: Justin Back <j.back@jrbit.de>

---
+++ cms/jrbit/class/crisp/models/HookFile.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace crisp\models;
+
+use Twig\Environment;
+
+abstract class HookFile
+{
+    public abstract function preRender(array $variables, Environment $TwigTheme, string $CurrentPage, string $CurrentFile): ?array;
+    public abstract function postRender(array $variables, Environment $TwigTheme, string $CurrentPage, string $CurrentFile): ?array;
+}
