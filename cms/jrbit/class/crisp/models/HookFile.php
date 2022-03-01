08fd6a68239de05c81ee105cbb7c5a0beb8cf8b5
Justin Back <j.back@jrbit.de>
2022-03-01 14:56:13 +0100

    Make preExecute and postExecute void
    Signed-off-by: Justin Back <j.back@jrbit.de>

--- cms/jrbit/class/crisp/models/HookFile.php
+++ cms/jrbit/class/crisp/models/HookFile.php
@@ -9,6 +9,6 @@ abstract class HookFile
     public abstract function preRender(array $variables, Environment $TwigTheme, string $CurrentPage, string $CurrentFile): ?array;
     public abstract function postRender(array $variables, Environment $TwigTheme, string $CurrentPage, string $CurrentFile): ?array;
 
-    public abstract function preExecute(string $Interface, Environment $TwigTheme): ?array;
-    public abstract function postExecute(string $Interface, Environment $TwigTheme): ?array;
+    public abstract function preExecute(string $Interface, Environment $TwigTheme): void;
+    public abstract function postExecute(string $Interface, Environment $TwigTheme): void;
 }
