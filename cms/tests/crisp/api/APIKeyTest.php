81246d7439e100f08a901ab3bc00a6e4e414c6c7
Justin Back <j.back@jrbit.de>
2022-09-05 11:12:19 +0200

    Add Unit Tests (WIP)

---
+++ cms/tests/crisp/api/APIKeyTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use crisp\api;
+use crisp\core\Postgres;
+use PDO;
+
+final class APIKeyTest extends TestCase
+{
+    public function testCreateApiKey(): void
+    {
+        $DB = (new Postgres())->getDBConnector();
+        
+        $statement = $DB->query("INSERT INTO APIKeys (key) VALUES ('12345')");
+
+        $this->assertTrue($statement->rowCount > 0);
+    }
+}
