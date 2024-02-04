931acc60dce7e4771e3e4e5709cbc6d10c883258
Justin Back <j.back@jrbit.de>
2024-02-04 00:26:25 +0000

    Update 02-migrations.sh

--- docker/02-migrations.sh
+++ docker/02-migrations.sh
@@ -6,8 +6,8 @@ sudo -E -u www-data crisp --migrate
 sudo -E -u www-data crisp theme --uninstall
 sudo -E -u www-data crisp theme --install
 sudo -E -u www-data crisp --clear-cache
-sudo -E -u www-data crisp theme --migrate | true # Allow to fail
-sudo -E -u www-data crisp theme --boot | true # Allow to fail
+sudo -E -u www-data crisp theme --migrate || true # Allow to fail
+sudo -E -u www-data crisp theme --boot || true # Allow to fail
 
 if [ ! -z "${PULL_LICENSE_ON_STARTUP}" ] || [ ! -z "${LICENSE_KEY}" ]; then
     sudo -E -u www-data crisp license --pull
