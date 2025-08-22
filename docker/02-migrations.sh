d4a9c42d8eb5940484aad2c53f2c0fd8a4a4c123
Justin Back <j.back@jrbit.de>
2025-08-22 03:12:33 +0200

    Install translations and storage on boot

--- docker/02-migrations.sh
+++ docker/02-migrations.sh
@@ -5,6 +5,8 @@ sudo -E -u www-data crisp crisp:migration:run --no-theme
 sudo -E -u www-data crisp crisp:check-permissions || true
 #sudo -E -u www-data crisp crisp:theme --uninstall || true # Disabled for performance testing
 sudo -E -u www-data crisp crisp:theme --install || true # Allow to fail
+sudo -E -u www-data crisp crisp:theme:translations --install || true # Allow to fail
+sudo -E -u www-data crisp crisp:theme:storage --install || true # Allow to fail
 sudo -E -u www-data crisp crisp:migration:run --no-core || true # Allow to fail
 sudo -E -u www-data crisp crisp:cache:clear
 sudo -E -u www-data crisp crisp:theme:execute-boot-files || true # Allow to fail
