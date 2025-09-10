09221138925b25bdc739fc16065184cff92d93b8
Justin Back <j.back@jrbit.de>
2025-09-10 04:30:07 +0200

    Rename CrispCMS to crisp framework in boot sequence

---
+++ docker/98-motd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+toilet "Crisp Framework"
+/usr/games/cowsay -f tux "... is ready to go!"
--- docker/99-entrypoint.sh
+++ docker/99-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-toilet "CrispCMS"
-/usr/games/cowsay -f tux "... is ready to go!"
-
-
 php-fpm -F -R -D || exit 1
 cron
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"
